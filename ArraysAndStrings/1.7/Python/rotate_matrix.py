ffd86c6b1128115946646136e38ea344ab584646
ChebaCheba <bastiangaxiola.com>
2019-12-04 15:02:04 -0600

    1.7 solved in place, python

--- ArraysAndStrings/1.7/Python/rotate_matrix.py
+++ ArraysAndStrings/1.7/Python/rotate_matrix.py
@@ -1,16 +1,24 @@
-"""def rotate_matrix(matrix):
-    n = len(matrix)
-    shifted = []
-    for i in range(n):
-        shifted.append([])
-        for j in range(n):
-            shifted[i].append(matrix[j][n-1-i])
-    return shifted
-matrix = [[0,1,2,3],
-         [4,5,6,7],
-         [8,9,0,1], 
-         [2,3,4,5]]
-print(rotate_matrix(matrix))"""
 def rotate_matrix(matrix):
     n = len(matrix)
-    pass
+    for i in range(n//2):
+        last = n - i - 1
+        for j in range(i, last):
+            offset = j - i
+            print(matrix)
+            top = matrix[i][j]
+            matrix[i][j] = matrix[last-offset][i]
+            matrix[last-offset][i] = matrix[last][last-j]
+            matrix[last][last-j] = matrix[i+j][last]
+            matrix[i+j][last] = top
+    return matrix
+
+if __name__=="__main__":
+    n = int(input("Introduce length of array 'n'\n"))
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(n):
+            matrix[i].append(int(input("Introduce Number\n")))
+    rotated = rotate_matrix(matrix)
+    print(rotated)
+
