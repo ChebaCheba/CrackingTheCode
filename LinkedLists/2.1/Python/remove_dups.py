08033567b15352f538bd295d9917755ce85081ea
ChebaCheba <bastiangaxiola.com>
2019-12-13 15:20:22 -0600

    add and delete functions added to structure, excercise 2.1 solved

--- LinkedLists/2.1/Python/remove_dups.py
+++ LinkedLists/2.1/Python/remove_dups.py
@@ -1,5 +1,26 @@
 import sys
 import os
-structure_path = os.path.split(os.path.split(sys.path[0])[0])[0]
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
-import linked_list
+
+sys.path.append(os.path.join("..",".."))
+from Structures.Python.linked_list import LinkedList
+
+def remove_dups(llist):
+    temp = set()
+    i = 0
+    while i < len(llist.nodes):
+        if llist.nodes[i].val not in temp:
+            temp.add(llist.nodes[i].val)
+            i+=1
+        else:
+            llist.delete_node_by_index(i)
+    return llist        
+
+if __name__=="__main__":
+    len_list = int(input("Introduce length of the linked list\n"))
+    llist = LinkedList()
+    for i in range(len_list):
+        node = int(input("Introduce value of node\n"))
+        llist.add_node(node)
+    llist.print_list()
+    llist = remove_dups(llist)
+    llist.print_list()
--- LinkedLists/Structures/Python/linked_list.py
+++ LinkedLists/Structures/Python/linked_list.py
@@ -25,6 +25,37 @@ class LinkedList:
             self.tail = self.nodes[-1]
         else:
             self.create([val])
+    def delete_node(self, val):
+        if self.nodes[0].val == val:
+            node = self.nodes[0]
+            node.next = None
+            self.nodes = self.nodes[1:]
+            self.head = self.nodes[0]
+            return node
+        elif self.nodes[len(self.nodes)-1].val == val:
+            self.tail = self.nodes[len(self.nodes)-2]
+            self.nodes[len(self.nodes)-2].next = None
+            return self.nodes.pop()
+        for i in range(1,len(self.nodes)-1):
+            if self.nodes[i].val == val:
+                node = self.nodes[i]
+                self.nodes[i-1].next = self.nodes[i+1]
+                node.next = None
+                self.nodes = self.nodes[:i]+self.nodes[i+1:]
+                return node
+        return None
+    def delete_node_by_index(self, index):
+        if index == 0 or index == (len(self.nodes)-1):
+            self.delete_node(self.nodes[index].val)
+        else:
+            try:
+                node = self.nodes[index]
+                node.next = None
+                self.nodes[index-1].next = self.nodes[index+1]
+                self.nodes = self.nodes[:index]+self.nodes[index+1:]            
+                return node
+            except:
+                return None
     def add_nodes(self, nodes):
         if self.nodes:
             new_node = Node(nodes[0])
