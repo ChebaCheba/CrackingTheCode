3bfb3ac803960919d030e98ea9564a4a316e0add
ChebaCheba <bastiangaxiola@gmail.com>
2020-06-23 00:36:48 -0500

    Solved 1.4 Golang

---
+++ ArraysAndStrings/1.4/Golang/palindrome_perm.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+)
+
+func is_palindrome(word string) bool {
+	wordS := strings.TrimSpace(word)
+	wordS = strings.ReplaceAll(wordS, " ", "")
+	temp := make(map[byte]struct{})
+	for i, _ := range wordS {
+		if _, ok := temp[wordS[i]]; ok {
+			delete(temp, wordS[i])
+		} else {
+			temp[wordS[i]] = struct{}{}
+		}
+	}
+	if len(temp) > 1 {
+		return false
+	}
+	return true
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Introduce a word")
+	word, _ := reader.ReadString('\n')
+
+	is := is_palindrome(word)
+	if is {
+		fmt.Println("It is a palindrome!")
+	} else {
+		fmt.Println("It is not a palindrome :(")
+	}
+}
